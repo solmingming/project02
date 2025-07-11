33f14d8a73e20d937cfc0795e5f90aa414129025
Seok Inho <easter0207@kaist.ac.kr>
2025-07-11 19:53:45 +0900

    wipe 1차 완성

---
+++ my-app/src/pages/01_IntroCanvas/components/Letter.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Letter = ({ char, x, y, rotation }) => {
+  const style = {
+    position: 'absolute',
+    left: `${x}px`,
+    top: `${y}px`,
+    transform: `rotate(${rotation}deg)`,
+    fontSize: '100px',
+    fontWeight: 'bold',
+    userSelect: 'none',
+  };
+
+  return <span style={style}>{char}</span>;
+};
+
+export default Letter;
