2372aa82bfb0dc999f7c5a56d75bde9110c70d52
Seok Inho <easter0207@kaist.ac.kr>
2025-07-14 14:52:52 +0900

    river 1차 완성 wipe 최종 완성

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import ButtonsPage from './pages/ButtonsPage';
+import WipePage from './pages/01_WipeCanvas/WipeCanvas';
+import RiverPage from './pages/03_RiverCanvas/RiverCanvas';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* 홈 (버튼 페이지만) */}
+        <Route path="/" element={<ButtonsPage />} />
+
+        {/* WipeCanvas 전용 페이지 */}
+        <Route path="/wipe" element={<WipePage />} />
+
+        {/* RiverCanvas 전용 페이지 */}
+        <Route path="/river" element={<RiverPage />} />
+      </Routes>
+    </Router>
   );
 }
 
